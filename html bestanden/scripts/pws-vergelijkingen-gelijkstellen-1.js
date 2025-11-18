02bdf21137892e289aed5b1005730b16b9053e3f
EvanBlaBla <cg3539@coornhert-gymnasium.nl>
2025-11-18 18:14:48 +0100

    paar aanpassingen

--- html bestanden/scripts/pws-vergelijkingen-gelijkstellen-1.js
+++ html bestanden/scripts/pws-vergelijkingen-gelijkstellen-1.js
@@ -65,8 +65,8 @@ function pwsVergelijkingenGelijkstellen1() {
     <br> Loek mag van zijn vader ${a+b} euro van zijn creditcard gebruiken. Elke dag (t) Koopt hij 1 nieuw spel van ${c} euro. Jonas heeft de creditcard van zijn moeder gekregen, waar hij ${b} euro op mag gebruiken. Hij geeft ${d} euro dagelijks uit aan snoep.<br><br>`
     document.querySelector('.js-opdracht1A').innerHTML = `
     <span class="js-score1A"></span>
-     - 1a <br> Loek mag van zijn vader ${a+b} euro van zijn creditcard gebruiken. Elke dag (t) Koopt hij 1 nieuw spel van ${c} euro. Jonas heeft de creditcard van zijn moeder gekregen, waar hij ${b} euro op mag gebruiken. Hij geeft ${d} euro dagelijks uit aan snoep.<br><br>
-    Stel de functies van Loek en Jonas op.`;
+    <p style= "margin: 0;">A) Stel de functies van Loek en Jonas op. Schrijf in de vorm: </p>
+    <p>yLoek = , yJonas =</p>`;
     //let resultaat1A = '';
     window.checken1A = function () {
       console.log("checken1A draait");
